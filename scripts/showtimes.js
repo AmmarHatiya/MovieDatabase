94a3890ea91bae790b02e25e120066fbe52cf8ed
ammarh <ammar.hatiya@gmail.com>
2022-04-11 06:38:45 -0400

    Finished part 1, got content from omdb api json file, and printed most reasonable values

---
+++ scripts/showtimes.js
@@ -0,0 +1,25 @@
+
+window.onload = function () {
+    let url = 'http://www.omdbapi.com/?i=tt3896198&apikey=821faf0d';
+
+    fetch(url)
+        .then(res => res.json())
+        .then((out) => {
+            console.log("URL is: ", url);
+            console.log('JSON:', out);
+            $('#title').val(out["Title"])
+            $('#year').val(out["Year"])
+            $('#genre').val(out["Genre"])
+            $('#runtime').val(out["Runtime"])
+            $('#director').val(out["Director"])
+            $('#writer').val(out["Writer"])
+            $('#title').val(out["Title"])
+            $('#title').val(out["Title"])
+            
+        })
+        .catch(err => {
+            throw err
+        });
+
+
+};
